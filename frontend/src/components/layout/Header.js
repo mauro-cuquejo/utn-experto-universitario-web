90f707416cb54d65eebff09e13b2a3fef92c1d42
mauro <mcuquejo@fi.uba.ar>
2023-04-13 13:59:11 -0300

    Se agrega modelo header transparente

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
     return (
         <header>
             <div className='holder header'>
-                <img src='images/printered.png' width={300} alt='h1 printered' />
+                <img src='images/printered-transparent.png' width={300} alt='h1 printered' />
             </div>
         </header>
     );
