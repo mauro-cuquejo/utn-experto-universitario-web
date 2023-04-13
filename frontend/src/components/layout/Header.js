ece81ff0e3061de4cf563c0a222abaf18f08e3e3
mauro <mcuquejo@fi.uba.ar>
2023-04-13 13:54:41 -0300

    Ajustes gráficos en header página

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -4,8 +4,7 @@ const Header = (props) => {
     return (
         <header>
             <div className='holder header'>
-                <img src='images/logo.png' width={100} alt='logo impresiones 3d' />
-                <img src='images/printered.png' width={200} alt='h1 printered' />
+                <img src='images/printered.png' width={300} alt='h1 printered' />
             </div>
         </header>
     );
