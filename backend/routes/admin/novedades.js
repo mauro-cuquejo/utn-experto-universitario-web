06e9f8f8ff30253044ef8098f675e6a7b5bd4d7b
mauro <mcuquejo@fi.uba.ar>
2023-05-28 23:20:38 -0300

    correccion get agregar. Se pasa a async. Creacion rutas para crud novedades

--- backend/routes/admin/novedades.js
+++ backend/routes/admin/novedades.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var helpers = require('../../helpers/helpers');
-var novedadesModel = require('../../models/novedadesModel');
+let express = require('express');
+let router = express.Router();
+let helpers = require('../../helpers/helpers');
+let novedadesModel = require('../../models/novedadesModel');
 
 router.get('/', async function (req, res, next) {
-    var novedades = await novedadesModel.getNovedades();
+    let novedades = await novedadesModel.getNovedades();
     res.render('admin/novedades', {
         layout: '/admin/layout',
         anio: await helpers.getAnio(),
@@ -13,10 +13,27 @@ router.get('/', async function (req, res, next) {
     });
 });
 
-router.get('/agregar', (req, res, next) => {
+router.get('/agregar', async (req, res, next) => {
     res.render('admin/agregar', {
-        layout: 'admin/layout'
+        layout: 'admin/layout',
+        anio: await helpers.getAnio(),
     });
+});
+
+router.get('/modificar/:id', async (req, res, next) => {
+    let id = req.params.id;
+    let novedad = await novedadesModel.getNovedadById(id);
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        anio: await helpers.getAnio(),
+        novedad
+    });
+});
+
+router.get('/eliminar/:id', async (req, res, next) => {
+    const id = req.params.id;
+    await novedadesModel.deleteNovedadById(id);
+    res.redirect('/admin/novedades');
 });
 
 router.post('/agregar', async (req, res, next) => {
@@ -27,6 +44,7 @@ router.post('/agregar', async (req, res, next) => {
         } else {
             res.render('admin/agregar', {
                 layout: 'admin/layout',
+                anio: await helpers.getAnio(),
                 error: true,
                 message: 'Todos los campos son requeridos (menos la imagen)'
             })
@@ -35,8 +53,40 @@ router.post('/agregar', async (req, res, next) => {
         console.log(error);
         res.render('admin/agregar', {
             layout: 'admin/layout',
+            anio: await helpers.getAnio(),
             error: true,
             message: 'No se cargó la novedad'
+        });
+    }
+});
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        let obj = {
+            titulo: req.body.titulo,
+            descripcion: req.body.descripcion,
+            precio: req.body.precio,
+            imagen: req.body.imagen
+        }
+        if (req.body.titulo != "" && req.body.descripcion != "" && req.body.precio != "") {
+            await novedadesModel.modificarNovedadById(obj, req.body.id);
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/modificar', {
+                layout: 'admin/layout',
+                anio: await helpers.getAnio(),
+                novedad: obj,
+                error: true,
+                message: 'Todos los campos son requeridos (menos la imagen)'
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            anio: await helpers.getAnio(),
+            error: true,
+            message: 'Error al modificar novedad'
         });
     }
 });
