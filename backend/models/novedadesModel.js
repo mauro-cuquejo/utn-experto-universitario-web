3084de8aa906629424c04aa52add62b38a4a08c5
mauro <mcuquejo@fi.uba.ar>
2023-05-28 23:19:46 -0300

    creacion funciones crud novedades

--- backend/models/novedadesModel.js
+++ backend/models/novedadesModel.js
@@ -1,15 +1,15 @@
-var pool = require('../bd');
+let pool = require('../bd');
 
 async function getNovedades() {
-    var query = "select * from novedades order by id desc";
-    var rows = await pool.query(query);
+    let query = "select * from novedades order by id desc";
+    let rows = await pool.query(query);
     return rows;
 }
 
 async function insertNovedad(obj) {
     try {
-        var query = "insert into novedades set ? ";
-        var rows = await pool.query(query, [obj]);
+        let query = "insert into novedades set ? ";
+        let rows = await pool.query(query, [obj]);
         return rows;
     } catch (error) {
         console.log(error);
@@ -17,4 +17,38 @@ async function insertNovedad(obj) {
     }
 }
 
-module.exports = { getNovedades, insertNovedad }
+async function deleteNovedadById(id) {
+    try {
+        let query = "delete from novedades where id = ? ";
+        let rows = await pool.query(query, [id]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function getNovedadById(id) {
+    try {
+        let query = "select * from novedades where id = ? ";
+        let rows = await pool.query(query, [id]);
+
+        return rows[0];
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function modificarNovedadById(obj, id) {
+    try {
+        let query = "update novedades set ? where id = ? ";
+        let rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+module.exports = { getNovedades, insertNovedad, deleteNovedadById, getNovedadById, modificarNovedadById }
